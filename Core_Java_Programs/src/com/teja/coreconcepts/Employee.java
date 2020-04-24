5ee0f2bb3486f7f98b77dc6f89866b33a0cd8bff
TEJA <TEJA@DESKTOP-Q9SMKC4>
2020-04-24 12:16:39 +0530

    Commit the Java files

---
+++ Core_Java_Programs/src/com/teja/coreconcepts/Employee.java
@@ -0,0 +1,39 @@
+package com.teja.coreconcepts;
+
+public class Employee {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee emp=new Employee("Dharma");
+		emp.setName("Teja");
+		System.out.println(emp);
+		
+		String s=new String("Dharma");
+		String s1=s.concat("Teja");
+		System.out.println(s);
+		System.out.println(s1);
+	
+	}
+	
+}
+
+
+
